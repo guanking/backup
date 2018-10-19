2d8dc19c5ede64ad6be357ce2d3a23139ae93416
GuanjieCao <guanking19@126.com>
2018-10-19 09:37:29 +0800

    add ftp

---
+++ python/ftp/login.py
@@ -0,0 +1,29 @@
+import ftplib
+g_cfg = {"username": "admin", "password": "admin", "host": "10.121.51.141"}
+g_ftp = None
+
+
+def login():
+    global g_cfg
+    cfg = g_cfg
+    ftp = ftplib.FTP(cfg["host"])
+    ftp.login(cfg["username"], cfg["password"])
+    cur_path = ftp.pwd()
+    print("cur path:" + cur_path)
+    global g_ftp
+    g_ftp = ftp
+
+
+def login_out():
+    global g_ftp
+    ftp = g_ftp
+    ftp.quit()
+    g_ftp=None
+
+if __name__ == '__main__':
+    print(g_cfg)
+    print("open")
+    login()
+    g_ftp.dir()
+    login_out()
+    print("close")
---
+++ python/ftp/transfile.py
@@ -0,0 +1,52 @@
+import login as lg
+import os
+g_ftp = None
+g_local_dir = "/home/c02804/tmp/"
+
+
+def download(files):
+    global g_ftp
+    if g_ftp is None:
+        print("ftp closed")
+        return
+    f_len = len(files)
+    buf_size = 1024
+    for i in range(0, f_len):
+        local_file = g_local_dir + files[i]
+        fp = open(local_file, "wb")
+        print("download %s" % files[i])
+        data_len = 0
+
+        def cb(data):
+            nonlocal data_len
+            data_len += fp.write(data)
+            os.system("clear")
+            print("download %s :%dB" % (files[i], data_len))
+
+        g_ftp.retrbinary("RETR %s" % files[i], cb, buf_size)
+        fp.close()
+
+
+def upload(files):
+    global g_ftp
+    if g_ftp is None:
+        print("ftp closed")
+        return
+    f_len=len(files)
+    buf_size = 1024
+    for i in range(0, f_len):
+        file_remote = files[i]
+        local_file = g_local_dir + files[i]
+        fp = open(local_file, 'rb')
+        print("upload %s" % files[i])
+        g_ftp.storbinary('STOR %s' % file_remote, fp, buf_size)
+        fp.close()
+
+if __name__ == '__main__':
+    print("transfile")
+    lg.login()
+    g_ftp = lg.g_ftp
+    upload(["smb.conf"])
+    # download(["smb.conf"])
+    lg.login_out()
+    print("finish")
